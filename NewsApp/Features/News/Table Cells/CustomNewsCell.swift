48d51cc906eed78e6eff328ec1e2c14e9a03e0b7
Madawi Ahmed <79159873+MadawiAh@users.noreply.github.com>
2022-10-26 16:43:13 +0300

    Fixing Favourites bug
    * favourites button now resets whenever the cell is reused.

--- NewsApp/Features/News/Table Cells/CustomNewsCell.swift
+++ NewsApp/Features/News/Table Cells/CustomNewsCell.swift
@@ -89,12 +89,18 @@ class CustomNewsCell: UITableViewCell {
         favouriteBtn.imageView?.heightAnchor.constraint(equalToConstant: 25).isActive = true
     }
     
+    private func resetFavouritesBtn(){
+        favouriteBtn.imageView?.image = UIImage(systemName: "suit.heart")
+        favouriteBtn.tintColor = theme.color.grayLightColor9fa1a1
+    }
+    
     // MARK: - Public Helpers
     
     func setNews(news: NewsData, shareTapped: ((CustomNewsCell)->())?, refreshTable: (()->())?){
         shareTappedClosure = shareTapped
         newsTitle.text = news.headline.main
         newsPublishTime.text = "\(news.formatedDate) • \(news.timeToRead)"
+        resetFavouritesBtn()
         
         newsImage.isHidden = false
         if news.multimedia.isEmpty {
