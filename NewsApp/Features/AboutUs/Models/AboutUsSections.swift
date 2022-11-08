91cdb9aad9f7aa3e1c43de0f93450607d2b41e9e
Madawi Ahmed <79159873+MadawiAh@users.noreply.github.com>
2022-11-08 15:05:07 +0300

    Improving a hyperlink range

--- NewsApp/Features/AboutUs/Models/AboutUsSections.swift
+++ NewsApp/Features/AboutUs/Models/AboutUsSections.swift
@@ -40,7 +40,7 @@ enum AboutUsSections: Int, CaseIterable {
         case .intro:
             return NSMutableAttributedString(string:"Daily News is an app that provides the option to browse, and save your favorite news, movie reviews , and books. ")
         case .overview:
-            return "\u{2022} Daily News was developed by Madawi with the guidance of a wonderful senior co-workers, with an intention to have the greatest amount of exposure to new skills and techniques in iOS development.\n\n\u{2022} Its important to mention that Daily News may ask you for the permission to access the “Camera” and “Photo album” for you to be able to update the profile picture. ".formatHypelinks(ranges: ["Madawi"])
+            return "\u{2022} Daily News was developed by Madawi with the guidance of a wonderful senior co-workers, with an intention to have the greatest amount of exposure to new skills and techniques in iOS development.\n\n\u{2022} Its important to mention that Daily News may ask you for the permission to access the “Camera” and “Photo album” for you to be able to update the profile picture. ".formatHypelinks(ranges: ["by Madawi"])
         case.credits:
             return "\u{2022} Daily News displayed materials are taken from The New York Times Developer Network APIs.\n\n\u{2022} All materials are protected by the copyright law, and they cannot be altered without a prior written permission from the New York Times company.\n\n\u{2022} Daily News animations are taken from LottieFiles.com".formatHypelinks(ranges: ["The New York Times Developer Network APIs","LottieFiles.com"])
         }
